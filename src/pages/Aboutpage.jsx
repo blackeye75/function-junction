26e1c55f6ebfa311f57b28ac3e8c9d7cc0461e49
blackeye75 <priyanshuraj22275@gmail.com>
2025-02-09 00:02:04 +0530

    add SmoothScrollHero component and update About page; enhance NewAbout layout with additional heading

--- src/pages/Aboutpage.jsx
+++ src/pages/Aboutpage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import About from '../component/about/About'
 import NewAbout from '../component/about/NewAbout'
+import { SmoothScrollHero } from '../component/about/SmoothScrollHero'
 
 const Aboutpage = () => {
     return (
         <div className='mt-16' >
             {/* <About /> */}
             <NewAbout/>
+            <SmoothScrollHero />
         </div>
     )
 }
